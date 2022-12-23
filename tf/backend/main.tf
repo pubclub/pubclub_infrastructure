e7c7c9454214248fe664bf4c5746613d16f478ec
Ed Morrell <morrell53@Eds-MacBook-Pro.local>
2022-12-23 11:35:12 +0000

    Added resource to create a ratings dynamodb table

--- tf/backend/main.tf
+++ tf/backend/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "4.48.0"
     }
   }
   backend "s3" {
@@ -14,4 +15,50 @@ terraform {
 provider "aws" {
   region                   = "eu-west-2"
   shared_credentials_files = ["$HOME/.aws/credentials"]
+}
+
+resource "aws_dynamodb_table" "ratings-table" {
+  name           = "ratings-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "RatingId"
+  range_key      = "CreationDate"
+
+  attribute {
+    name = "RatingId"
+    type = "S"
+  }
+
+  attribute {
+    name = "CreationDate"
+    type = "S"
+  }
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "PlaceId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "UserIdIndex"
+    hash_key        = "UserId"
+    projection_type = "ALL"
+    read_capacity   = 5
+    write_capacity  = 5
+  }
+
+  global_secondary_index {
+    name            = "PlaceIdIndex"
+    hash_key        = "PlaceId"
+    projection_type = "ALL"
+    read_capacity   = 5
+    write_capacity  = 5
+  }
+
 }
